8e924f5fd0f6eaf75cf71147458478fffbb51980
Yihui Xie <xie@yihui.name>
2017-11-28 00:22:16 -0600

    silent curl

--- tools/install-macos.sh
+++ tools/install-macos.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 cd $TMPDIR
-curl -LO https://github.com/yihui/tinytex/raw/master/tools/texlive.profile
-curl -L https://github.com/yihui/tinytex/raw/master/tools/install-base.sh | sh
+curl -sLO https://github.com/yihui/tinytex/raw/master/tools/texlive.profile
+curl -sL https://github.com/yihui/tinytex/raw/master/tools/install-base.sh | sh
 rm -r install-tl-* texlive.profile
 
 TEXDIR=~/Library/TinyTeX
 mkdir -p $TEXDIR
 mv texlive/* $TEXDIR
+rm -r texlive
 $TEXDIR/bin/*/tlmgr path add
 
-curl -L https://github.com/yihui/tinytex/raw/master/tools/install-recommended.sh | sh
+curl -sL https://github.com/yihui/tinytex/raw/master/tools/install-recommended.sh | sh
 tlmgr path add
