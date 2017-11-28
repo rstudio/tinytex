763825f87078eb43788ba0d79f38eef1e19cf38b
Yihui Xie <xie@yihui.name>
2017-11-28 09:41:39 -0600

    support Linux

--- tools/install-macos.sh
+++ tools/install-macos.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+if [[ ! -d $TMPDIR ]]; then
+  TMPDIR=/tmp
+fi
 cd $TMPDIR
 curl -sLO https://github.com/yihui/tinytex/raw/master/tools/texlive.profile
 curl -sL https://github.com/yihui/tinytex/raw/master/tools/install-base.sh | sh
 rm -r install-tl-* texlive.profile
 
-TEXDIR=~/Library/TinyTeX
+if [[ $(uname) == 'Darwin' ]]; then
+  TEXDIR=~/Library/TinyTeX
+else
+  TEXDIR=~/.TinyTeX
+fi
+
 rm -rf $TEXDIR
 mkdir -p $TEXDIR
 mv texlive/* $TEXDIR
