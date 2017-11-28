802af08b69b2ac2f7bff1df105f67e0b4026b0c9
Yihui Xie <xie@yihui.name>
2017-11-28 00:06:01 -0600

    install some "recommended" packages (recommended by me from experience)

---
+++ tools/install-recommended.sh
@@ -0,0 +1,1 @@
+tlmgr install amscls amsfonts amsmath bibtex booktabs caption dvipdfmx ec etoolbox fancyvrb fontspec framed geometry graphics graphics-def gsftopk hyperref ifluatex ifxetex lm luaotfload makeindex mathspec natbib oberdiek titling tools upquote url xkeyval zapfding
