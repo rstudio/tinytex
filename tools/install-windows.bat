f1482b050d2ed1ed1e5885f100a02efe618a6b21
Yihui Xie <xie@yihui.name>
2017-11-30 09:07:37 -0600

    add some comments to the windowns install script

--- tools/install-windows.bat
+++ tools/install-windows.bat
@@ -3,26 +3,37 @@ cd /d "%TMP%"
 cd /d "%TEMP%"
 cd /d "%TMPDIR%"
 
+rem in case there is a leftover install-tl-* dir, delete it
 for /d %%G in ("install-tl-*") do rd /s /q "%%~G"
+
+rem download install-tl.zip and unzip it
 powershell -Command "Invoke-WebRequest http://mirror.ctan.org/systems/texlive/tlnet/install-tl.zip -OutFile install-tl.zip"
 powershell -Command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('install-tl.zip', '.'); }"
 del install-tl.zip
+
+rem download texlive.profile and modify it (set texdir to ./TinyTeX)
 powershell -Command "Invoke-WebRequest https://github.com/yihui/tinytex/raw/master/tools/texlive.profile -OutFile texlive.profile"
 powershell -Command "(gc texlive.profile) -replace './', './TinyTex/' | Out-File -encoding ASCII texlive.profile"
+
+rem download the custom package list
 powershell -Command "Invoke-WebRequest https://github.com/yihui/tinytex/raw/master/tools/pkgs-custom.txt -OutFile pkgs-custom.txt"
 
+rem an automated installation of TeXLive (infrastructure only)
 cd install-tl-*
 @echo | install-tl-windows.bat -profile=../texlive.profile
 
 del TinyTeX\install-tl.log ..\texlive.profile
 
+rem TeXLive installed to ./TinyTeX; move it to APPDATA
 rd /s /q "%APPDATA%\TinyTeX"
 rd /s /q "%APPDATA%\TinyTeX"
 move /y TinyTeX "%APPDATA%"
 
+rem clean up the install-tl-* directory
 cd ..
 for /d %%G in ("install-tl-*") do rd /s /q "%%~G"
 
+rem install all custom packages
 @echo off
 setlocal enabledelayedexpansion
 set "pkgs="
@@ -33,4 +44,5 @@ del pkgs-custom.txt
 
 "%APPDATA%\TinyTeX\bin\win32\tlmgr" path add && "%APPDATA%\TinyTeX\bin\win32\tlmgr" install latex-bin xetex %pkgs%
 
+rem TODO: the above line will make this batch file exit prematurely, but I don't know why
 pause
