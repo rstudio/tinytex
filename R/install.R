b3294a14c8fe83284ece4c11f56d6f8e0f76ac78
Yihui Xie <xie@yihui.name>
2017-12-01 22:47:13 -0600

    add a function uninstall_tinytex()

--- R/install.R
+++ R/install.R
@@ -1,23 +1,44 @@
-#' Install TinyTeX
+#' Install/Uninstall TinyTeX
 #'
-#' Download the installation script from \url{https://github.com/yihui/tinytex}
-#' according to the platform (Unix or Windows), and execute it to install
-#' TinyTeX.
+#' The function \code{install_tinytex()} downloads the installation script from
+#' \url{https://github.com/yihui/tinytex} according to the platform (Unix or
+#' Windows), and executes it to install TinyTeX (a custom LaTeX distribution
+#' based on TeX Live). The function \code{uninstall_tinytex()} removes TinyTeX.
 #' @references TinyTeX homepage: \url{https://yihui.name/tinytex/}.
 #' @export
 install_tinytex = function() {
   owd = setwd(tempdir()); on.exit(setwd(owd), add = TRUE)
-  if (.Platform$OS.type == 'unix') {
-    download.file(
-      'https://github.com/yihui/tinytex/raw/master/tools/install-unx.sh',
-      'install-unx.sh'
-    )
-    system2('sh', 'install-unix.sh')
-  } else {
-    download.file(
-      'https://github.com/yihui/tinytex/raw/master/tools/install-windows.bat',
-      'install-windows.bat'
-    )
-    system2('install-windows.bat', invisible = FALSE)
-  }
+  switch(
+    .Platform$OS.type,
+    'unix' = {
+      download.file(
+        'https://github.com/yihui/tinytex/raw/master/tools/install-unx.sh',
+        'install-unx.sh'
+      )
+      system2('sh', 'install-unix.sh')
+    },
+    'windows' = {
+      download.file(
+        'https://github.com/yihui/tinytex/raw/master/tools/install-windows.bat',
+        'install-windows.bat'
+      )
+      system2('install-windows.bat', invisible = FALSE)
+    },
+    stop('This platform is not supported.')
+  )
+}
+
+#' @rdname install_tinytex
+#' @export
+uninstall_tinytex = function() {
+  target = switch(
+    .Platform$OS.type,
+    'windows' = file.path(
+      Sys.getenv('APPDATA', stop('Environment variable "APPDATA" not set.')), 'TinyTeX'
+    ),
+    'unix' = if (Sys.info()[['sysname']] == 'Darwin') '~/Library/TinyTeX' else '~/.TinyTeX',
+    stop('This platform is not supported.')
+  )
+  tlmgr_path('remove')
+  unlink(target, recursive = TRUE)
 }
