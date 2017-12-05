4ae215412f095ced8e917e851f558356ae620417
Yihui Xie <xie@yihui.name>
2017-12-05 11:27:21 -0600

    utils::winDialog() has a limit of 255 characters on the message; I love Windows

--- R/install.R
+++ R/install.R
@@ -51,12 +51,11 @@ install_tinytex = function() {
       unzip('install-tl.zip')
       local({
         owd = setwd(list.files('.', '^install-tl-.*')); on.exit(setwd(owd), add = TRUE)
+        message('Starting to install TinyTeX to ', appdata, '. It will take a few minutes.')
         (if (interactive()) function(msg) utils::winDialog('ok', msg) else message)(paste0(
-          'Starting to install TinyTeX to ', appdata,
-          '. It will take a few minutes.\n\n',
-          'You may see two error dialog boxes about the missing luatex.dll during the installation, ',
-          ' and an error message like "Use of uninitialized value in bitwise or (|)..." at the end. ',
-          'These messages can be safely ignored. When installation is complete, ',
+          'Next you may see two error dialog boxs about the missing luatex.dll, ',
+          'and an error message like "Use of uninitialized value in bitwise or (|)..." at the end. ',
+          'These messages can be ignored. When installation is complete, ',
           'please restart ', if (Sys.getenv('RSTUDIO') != '') 'RStudio' else 'R', '.'
         ))
         shell('echo | install-tl-windows.bat -profile=../texlive.profile', invisible = FALSE)
