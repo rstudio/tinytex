8f224d9ed1a1dc766da8b795a8961b83b3fc0f2f
Yihui Xie <xie@yihui.name>
2018-07-02 12:24:41 -0500

    download and install the prebuilt TinyTeX on Travis

---
+++ tools/download-travis-linux.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mkdir -p ~/bin
+
+[ ! -z $TINYTEX_FORCE_INSTALL ] && rm -rf ~/.TinyTeX
+
+if [ ! -d ~/.TinyTeX ]; then
+  wget -q https://travis-bin.yihui.name/tinytex.tar.gz
+  tar xzf tinytex.tar.gz -C ~/
+  rm tinytex.tar.gz
+fi
+
+cd ~/.TinyTeX/bin/*/
+./tlmgr path add
