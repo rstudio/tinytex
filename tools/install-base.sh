941886b1a36b378d714af8127557d2772f254fc0
Yihui Xie <xie@yihui.name>
2020-03-06 13:16:37 -0600

    try to fix #150: do not use $HOME in texmf variables

--- tools/install-base.sh
+++ tools/install-base.sh
@@ -13,12 +13,25 @@ else
   wget $PRURL
   # ask `tlmgr path add` to add binaries to ~/bin instead of the default
   # /usr/local/bin unless this script is invoked with the argument '--admin'
-  # (e.g., users want to make LaTeX binaries available system-wide)
-  if [ "$1" != '--admin' ]; then
+  # (e.g., users want to make LaTeX binaries available system-wide), in which
+  # case we personalize texmf variables
+  if [ "$1" == '--admin' ]; then
+    echo 'TEXMFCONFIG $HOME/.TinyTeX/texmf-config' >> tinytex.profile
+    echo 'TEXMFHOME $HOME/.TinyTeX/texmf-home' >> tinytex.profile
+    echo 'TEXMFVAR $HOME/.TinyTeX/texmf-var' >> tinytex.profile
+  else
     mkdir -p $HOME/bin
     echo "tlpdbopt_sys_bin ${HOME}/bin" >> tinytex.profile
   fi
 fi
+
+# no need to personalize texmf variables if not installed by admin
+if [ "$1" != '--admin' ]; then
+  echo 'TEXMFCONFIG $TEXMFSYSCONFIG' >> tinytex.profile
+  echo 'TEXMFHOME $TEXMFLOCAL' >> tinytex.profile
+  echo 'TEXMFVAR $TEXMFSYSVAR' >> tinytex.profile
+fi
+
 tar -xzf install-tl-unx.tar.gz
 rm install-tl-unx.tar.gz
 
--- tools/tinytex.profile
+++ tools/tinytex.profile
@@ -3,13 +3,8 @@ selected_scheme scheme-infraonly
 TEXDIR ./
 
 TEXMFSYSCONFIG ./texmf-config
-TEXMFCONFIG $HOME/.TinyTeX/texmf-config
-
 TEXMFLOCAL ./texmf-local
-TEXMFHOME $HOME/.TinyTeX/texmf-home
-
 TEXMFSYSVAR ./texmf-var
-TEXMFVAR $HOME/.TinyTeX/texmf-var
 
 option_doc 0
 option_src 0
