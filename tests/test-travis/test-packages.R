4648d01ceae5d621bac68e16e9bf116d978960f6
Yihui Xie <xie@yihui.name>
2020-05-12 16:56:38 -0500

    install a minimal scheme of TeX Live, compile a bookdown demo to install missing LaTeX packages automatically, and compare the installed packages with the required packages to make sure the default installation has included all required packages (#207)

---
+++ tests/test-travis/test-packages.R
@@ -0,0 +1,19 @@
+# make sure the default installation includes all LaTeX packages required to
+# compile basic R Markdown documents and bookdown books to PDF
+if (.Platform$OS.type == 'unix') xfun::in_dir('../../tools', {
+  system('sh install-base.sh && ./texlive/bin/*/tlmgr path add')
+  xfun::pkg_load2('bookdown')
+  bookdown:::bookdown_skeleton('book')
+  xfun::in_dir('book', for (i in c('pdflatex', 'xelatex', 'lualatex')) {
+    bookdown::render_book(
+      'index.Rmd', 'bookdown::pdf_book', output_options = list(latex_engine = i)
+    )
+  })
+  x1 = sort(tinytex::tl_pkgs())
+  x2 = sort(readLines('pkgs-custom.txt'))
+  if (!identical(x1, x2)) stop(
+    'pkgs-custom.txt needs to be updated.\n\nPackages installed are:\n',
+    paste(x2, collapse = '\n'), '\n\nPackages required are:\n',
+    paste(x1, collapse = '\n')
+  )
+})
