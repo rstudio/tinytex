963f3b5fb04357105e5987e2122886455deb9010
Yihui Xie <xie@yihui.name>
2020-05-12 21:56:38 -0500

    remove debugging info

--- tests/test-travis/test-packages.R
+++ tests/test-travis/test-packages.R
@@ -1,9 +1,8 @@
 # make sure the default installation includes all LaTeX packages required to
 # compile basic R Markdown documents and bookdown books to PDF
-if (.Platform$OS.type == 'unix') xfun::in_dir('../../../tools', {
+if (FALSE && .Platform$OS.type == 'unix') xfun::in_dir('../../../tools', {
   system('sh install-base.sh && ./texlive/bin/*/tlmgr path add')
   unlink(normalizePath('~/texlive'), recursive = TRUE)
-  cat(list.files('texlive', full.names = TRUE, recursive = TRUE), sep = '\n')
   bookdown:::bookdown_skeleton('book')
   xfun::in_dir('book', for (i in c('pdflatex', 'xelatex', 'lualatex')) {
     bookdown::render_book(
