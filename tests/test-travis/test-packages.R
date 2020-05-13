b93ed5d1272b1f582b1bf798316721ba9686c287
Yihui Xie <xie@yihui.name>
2020-05-12 22:12:40 -0500

    retry building the bookdown project

--- tests/test-travis/test-packages.R
+++ tests/test-travis/test-packages.R
@@ -1,10 +1,10 @@
 # make sure the default installation includes all LaTeX packages required to
 # compile basic R Markdown documents and bookdown books to PDF
-if (FALSE && .Platform$OS.type == 'unix') xfun::in_dir('../../../tools', {
+if (.Platform$OS.type == 'unix') xfun::in_dir('../../../tools', {
   system('sh install-base.sh && ./texlive/bin/*/tlmgr path add')
   unlink(normalizePath('~/texlive'), recursive = TRUE)
   bookdown:::bookdown_skeleton('book')
-  xfun::in_dir('book', for (i in c('pdflatex', 'xelatex', 'lualatex')) {
+  for (i in c('pdflatex', 'xelatex', 'lualatex')) xfun::in_dir('book', {
     bookdown::render_book(
       'index.Rmd', 'bookdown::pdf_book', output_options = list(latex_engine = i),
       quiet = TRUE
