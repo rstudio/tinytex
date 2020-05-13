1cba6de85a7ec098759ac52eff7bcaa9715efa16
Yihui Xie <xie@yihui.name>
2020-05-12 20:28:27 -0500

    correct access to the tools directory

--- tests/test-travis/test-packages.R
+++ tests/test-travis/test-packages.R
@@ -1,6 +1,6 @@
 # make sure the default installation includes all LaTeX packages required to
 # compile basic R Markdown documents and bookdown books to PDF
-if (.Platform$OS.type == 'unix') xfun::in_dir('../../tools', {
+if (.Platform$OS.type == 'unix') xfun::in_dir('../../../tinytex/tools', {
   system('sh install-base.sh && ./texlive/bin/*/tlmgr path add')
   xfun::pkg_load2('bookdown')
   bookdown:::bookdown_skeleton('book')
