1adc5119f58f4aff948497fc942bdd436ae66c8d
Yihui Xie <xie@yihui.name>
2020-05-13 13:31:45 -0500

    render a basic Rmd document, and check which new LaTeX packages are required besides the very basic packages

--- tests/test-travis/test-packages.R
+++ tests/test-travis/test-packages.R
@@ -3,13 +3,19 @@
 if (.Platform$OS.type == 'unix') xfun::in_dir('../../../tools', {
   system('sh install-base.sh && ./texlive/bin/*/tlmgr path add')
   unlink(normalizePath('~/texlive'), recursive = TRUE)
+  x0 = tinytex::tl_pkgs()  # packages from the infraonly scheme
+
+  # render some Rmd files to automatically install LaTeX packages to TinyTeX
+  rmarkdown::render('test-basic.Rmd', 'pdf_document')
   bookdown:::bookdown_skeleton('book')
   xfun::in_dir('book', for (i in c('pdflatex', 'xelatex', 'lualatex')) {
     x = readLines('_output.yml')
     writeLines(gsub('^(\\s+latex_engine:).+$', paste('\\1', i), x), '_output.yml')
     bookdown::render_book('index.Rmd', 'bookdown::pdf_book', quiet = TRUE)
   })
-  x1 = sort(tinytex::tl_pkgs())
+
+  # now see which packages are required to compile the above Rmd files
+  x1 = sort(setdiff(tinytex::tl_pkgs(), x0))
   x2 = sort(readLines('pkgs-custom.txt'))
   if (!identical(x1, x2)) stop(
     'pkgs-custom.txt needs to be updated.\n\nPackages required are:\n',
