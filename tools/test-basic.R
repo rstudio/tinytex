2c3072c31652e9c982a554976832e029ad7dc917
Yihui Xie <xie@yihui.name>
2020-05-13 20:34:32 -0500

    turn on the verbose mode

--- tools/test-basic.R
+++ tools/test-basic.R
@@ -1,4 +1,5 @@
 # make sure a basic Rmd document compiles with TinyTeX
+options(tinytex.verbose = TRUE)
 for (i in c('pdflatex', 'xelatex', 'lualatex')) {
   for (j in c('pdf_document', 'beamer_presentation')) {
     rmarkdown::render('tools/test-basic.Rmd', j, output_options = list(latex_engine = i))
