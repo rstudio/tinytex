b8bdaa2103fa3c1db708f4389868c06eb6a81b38
Yihui Xie <xie@yihui.name>
2020-05-15 22:46:58 -0500

    change the wget progress bar style (less stdout)

--- tools/download-travis-linux.sh
+++ tools/download-travis-linux.sh
@@ -5,7 +5,7 @@ mkdir -p ~/bin
 [ ! -z $TINYTEX_FORCE_INSTALL ] && rm -rf ~/.TinyTeX
 
 if [ ! -d ~/.TinyTeX/bin ]; then
-  wget https://travis-bin.yihui.org/tinytex.tar.gz
+  wget --progress=dot:giga https://travis-bin.yihui.org/tinytex.tar.gz
   tar xzf tinytex.tar.gz -C ~/
   rm tinytex.tar.gz
 fi
