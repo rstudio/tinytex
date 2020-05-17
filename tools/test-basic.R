23943cf40ea5ef88469d0ad7b45c78eb82f3c5ac
Yihui Xie <xie@yihui.name>
2020-05-16 23:40:38 -0500

    make sure rmarkdown is installed

--- tools/test-basic.R
+++ tools/test-basic.R
@@ -6,6 +6,7 @@ tinytex::tlmgr_install(readLines('tools/pkgs-yihui.txt'))
 
 # make sure a basic Rmd document compiles with TinyTeX
 options(tinytex.verbose = TRUE)
+xfun::pkg_load2('rmarkdown')
 for (i in c('pdflatex', 'xelatex', 'lualatex')) {
   for (j in c('pdf_document', 'beamer_presentation')) {
     rmarkdown::render('tools/test-basic.Rmd', j, output_options = list(latex_engine = i))
