aab9e11ef1c611a0d73070d6428dd5611e603408
Yihui Xie <xie@yihui.name>
2020-05-21 14:56:05 -0500

    delete files generated from latex compilation (e.g. cache files)

--- tools/test-basic.R
+++ tools/test-basic.R
@@ -3,6 +3,10 @@ for (i in c('tlmgr', 'pdflatex', 'xelatex', 'pandoc')) {
   system2(i, '--version')
 }
 tinytex::tlmgr_install(readLines('tools/pkgs-yihui.txt'))
+all_files = function() {
+  list.files(tinytex::tinytex_root(), full.names = TRUE, recursive = TRUE)
+}
+files_old = all_files()
 
 # make sure a basic Rmd document compiles with TinyTeX
 options(tinytex.verbose = TRUE)
@@ -11,4 +15,12 @@ for (i in c('pdflatex', 'xelatex', 'lualatex')) {
   for (j in c('pdf_document', 'beamer_presentation')) {
     rmarkdown::render('tools/test-basic.Rmd', j, output_options = list(latex_engine = i))
   }
+}
+
+if (length(files_new <- setdiff(all_files(), files_old))) {
+  message(
+    'Deleting files created during LaTeX compilation...\n',
+    paste('  -', files_new, collapse = '\n')
+  )
+  file.remove(files_new)
 }
