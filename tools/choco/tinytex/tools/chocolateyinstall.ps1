aff18764e42de4db509180bde9619b922c4e2405
Syrus Dark <naveen@syrusdark.website>
2020-08-17 14:12:54 +0530

    CheckSum are not nice and changes each time.

--- tools/choco/tinytex/tools/chocolateyinstall.ps1
+++ tools/choco/tinytex/tools/chocolateyinstall.ps1
@@ -8,9 +8,7 @@ $pkgcustom = "https://yihui.org/gh/tinytex/tools/pkgs-custom.txt"
 $installTLFileLoc = Get-ChocolateyWebFile `
   -PackageName "texlive" `
   -FileFullPath "$toolsDir\install-tl.zip" `
-  -Url $texliveInstall `
-  -Checksum "1a7601c1a33276731a56aba24e55b775eb2a6e417f24dd106f8ead5ad432b10b0bd80947ddbe85840e761887b7c10b84f5218d61b3640900998734b71b6c4b61" `
-  -ChecksumType sha512
+  -Url $texliveInstall
 
 Get-ChocolateyUnzip `
   -FileFullPath $installTLFileLoc `
