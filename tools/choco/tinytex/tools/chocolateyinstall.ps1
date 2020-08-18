f1e68164d3a3a0505091edd8761476d00517ec19
Syrus Dark <naveen@syrusdark.website>
2020-08-18 14:02:03 +0530

    Try Get-WebFile

--- tools/choco/tinytex/tools/chocolateyinstall.ps1
+++ tools/choco/tinytex/tools/chocolateyinstall.ps1
@@ -5,9 +5,8 @@ $texliveInstall="http://mirrors.ctan.org/systems/texlive/tlnet/install-tl.zip"
 $profileUrl = "https://yihui.org/gh/tinytex/tools/tinytex.profile"
 $pkgcustom = "https://yihui.org/gh/tinytex/tools/pkgs-custom.txt"
 
-$installTLFileLoc = Get-ChocolateyWebFile `
-  -PackageName "texlive" `
-  -FileFullPath "$toolsDir\install-tl.zip" `
+$installTLFileLoc = Get-WebFile `
+  -FileName "$toolsDir\install-tl.zip" `
   -Url $texliveInstall
 
 Get-ChocolateyUnzip `
@@ -19,21 +18,19 @@ Get-ChocolateyUnzip `
 Remove-Item $installTLFileLoc
 
 #download tinytex.profile and modify it (set texdir to ./TinyTeX)
-Get-ChocolateyWebFile `
-  -PackageName "texlive" `
-  -FileFullPath "$toolsDir\tinytex.profile" `
+Get-WebFile `
+  -FileName "$toolsDir\tinytex.profile" `
   -Url $profileUrl `
 (gc "$toolsDir\tinytex.profile") -replace '\./', './TinyTex/' | Out-File -encoding ASCII tinytex.profile
 
 #I don't the the powershell command for this
 cmd.exe /C echo TEXMFCONFIG $TEXMFSYSCONFIG>> tinytex.profile
-cmd.exe /C TEXMFHOME $TEXMFLOCAL>> tinytex.profile
-cmd.exe /C TEXMFVAR $TEXMFSYSVAR>> tinytex.profile
+cmd.exe /C echo TEXMFHOME $TEXMFLOCAL>> tinytex.profile
+cmd.exe /C echo TEXMFVAR $TEXMFSYSVAR>> tinytex.profile
 
 #download the custom package list
-Get-ChocolateyWebFile `
-  -PackageName "texlive" `
-  -FileFullPath "$toolsDir\pkgs-custom.txt" `
+Get-WebFile `
+  -FileName "$toolsDir\pkgs-custom.txt" `
   -Url $pkgcustom `
 
 #an automated installation of TeXLive (infrastructure only)
