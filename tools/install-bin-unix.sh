9ba0e4eef2ea98b8a3cef4eff65bd91c51c7e4ab
Yihui Xie <xie@yihui.name>
2020-09-15 17:02:25 -0500

    delete the existing installation

--- tools/install-bin-unix.sh
+++ tools/install-bin-unix.sh
@@ -4,11 +4,13 @@ cd ${TMPDIR:-/tmp}
 
 if [ $(uname) = 'Darwin' ]; then
   curl -LO https://yihui.org/tinytex/TinyTeX-1.tgz
-  tar xzf TinyTeX-1.tar.gz -C ~/Library/
+  rm -r ~/Library/TinyTeX
+  tar xzf TinyTeX-1.tgz -C ~/Library/
   rm TinyTeX-1.tgz
   ~/Library/TinyTeX/bin/*/tlmgr path add
 else
   wget --progress=dot:giga https://yihui.org/tinytex/TinyTeX-1.tar.gz
+  rm -r ~/.TinyTeX
   tar xzf TinyTeX-1.tar.gz -C ~/
   rm TinyTeX-1.tar.gz
   cd ~/.TinyTeX/bin/*/
