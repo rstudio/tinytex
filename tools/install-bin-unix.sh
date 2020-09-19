48f3500531040239f4bc65b02e8da3f33284adbb
Yihui Xie <xie@yihui.name>
2020-09-18 23:22:57 -0500

    support install from source on *nix platforms that are not macOS or Linux

--- tools/install-bin-unix.sh
+++ tools/install-bin-unix.sh
@@ -2,18 +2,40 @@
 
 cd ${TMPDIR:-/tmp}
 
-if [ $(uname) = 'Darwin' ]; then
-  curl -LO https://yihui.org/tinytex/TinyTeX-1.tgz
-  rm -r ~/Library/TinyTeX
-  tar xzf TinyTeX-1.tgz -C ~/Library/
-  rm TinyTeX-1.tgz
-  ~/Library/TinyTeX/bin/*/tlmgr path add
+OSNAME=$(uname)
+[ -z $OSNAME ] && echo "This operating system is not supported." && exit 1
+
+if [ $OSNAME = 'Darwin' ]; then
+  TEXDIR=${TINYTEX_DIR:-~/Library}/TinyTeX
 else
-  wget --progress=dot:giga https://yihui.org/tinytex/TinyTeX-1.tar.gz
-  rm -r ~/.TinyTeX
-  tar xzf TinyTeX-1.tar.gz -C ~/
-  rm TinyTeX-1.tar.gz
-  cd ~/.TinyTeX/bin/*/
-  ./tlmgr option sys_bin ~/bin
-  ./tlmgr path add
+  TEXDIR=${TINYTEX_DIR:-~}/.TinyTeX
 fi
+
+rm -rf $TEXDIR
+
+case $OSNAME in
+  "Darwin")
+    curl -LO https://yihui.org/tinytex/TinyTeX-1.tgz
+    tar xzf TinyTeX-1.tgz -C $(dirname $TEXDIR)
+    rm TinyTeX-1.tgz
+    ;;
+  "Linux")
+    wget --progress=dot:giga https://yihui.org/tinytex/TinyTeX-1.tar.gz
+    tar xzf TinyTeX-1.tar.gz -C $(dirname $TEXDIR)
+    rm TinyTeX-1.tar.gz
+    ;;
+  *)
+    echo "We do not have a prebuilt TinyTeX package for the operating system $(uname)."
+    echo "I will try to install from source for you instead."
+    wget https://yihui.org/tinytex/installer-unix.tar.gz
+    tar xzf installer-unix.tar.gz
+    ./install.sh
+    mkdir -p $TEXDIR
+    mv texlive/* $TEXDIR
+    rm -r texlive installer-unix.tar.gz install.sh
+    ;;
+esac
+
+cd $TEXDIR/bin/*/
+[ $OSNAME != "Darwin" ] && ./tlmgr option sys_bin ~/bin
+./tlmgr path add
