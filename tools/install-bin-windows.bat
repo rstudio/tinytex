87fffd25d7ee1a10e5d9924b6380fe9927b63472
Yihui Xie <xie@yihui.name>
2020-09-19 22:47:51 -0500

    add the extension .zip to the URL

--- tools/install-bin-windows.bat
+++ tools/install-bin-windows.bat
@@ -10,13 +10,13 @@ for /d %%G in ("TinyTeX*") do rd /s /q "%%~G"
 if not defined TINYTEX_INSTALLER set TINYTEX_INSTALLER=TinyTeX-1
 
 if not defined TINYTEX_VERSION (
-  set TINYTEX_URL=https://yihui.org/tinytex/%TINYTEX_INSTALLER%
+  set TINYTEX_URL=https://yihui.org/tinytex/%TINYTEX_INSTALLER%.zip
 ) else (
-  set TINYTEX_URL=https://github.com/yihui/tinytex-releases/releases/download/v%TINYTEX_VERSION%/%TINYTEX_INSTALLER%-v%TINYTEX_VERSION%
+  set TINYTEX_URL=https://github.com/yihui/tinytex-releases/releases/download/v%TINYTEX_VERSION%/%TINYTEX_INSTALLER%-v%TINYTEX_VERSION%.zip
 )
 
 rem download the zip package and unzip it
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $($Env:TINYTEX_URL).zip -OutFile install.zip"
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $Env:TINYTEX_URL -OutFile install.zip"
 powershell -Command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('install.zip', '.'); }"
 del install.zip
 
