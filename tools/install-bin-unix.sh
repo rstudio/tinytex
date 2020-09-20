443a969f2a5d08090f1c4695a78b41880a064d10
Yihui Xie <xie@yihui.name>
2020-09-19 22:00:34 -0500

    use the env var TINYTEX_INSTALLER to select the TinyTeX installer (e.g., TinyTeX-1), and TINYTEX_VERSION to select the TinyTeX version (e.g., 2020.09.19)

--- tools/install-bin-unix.sh
+++ tools/install-bin-unix.sh
@@ -11,34 +11,45 @@ cd ${TMPDIR:-/tmp}
 OSNAME=$(uname)
 [ -z $OSNAME ] && echo "This operating system is not supported." && exit 1
 
+TINYTEX_INSTALLER=${TINYTEX_INSTALLER:-"TinyTeX-1"}
+
 if [ $OSNAME = 'Darwin' ]; then
-  TEXDIR=${TINYTEX_DIR:-~/Library}/TinyTeX
+  TEXDIR=${TINYTEX_DIR:-"~/Library"}/TinyTeX
 else
-  TEXDIR=${TINYTEX_DIR:-~}/.TinyTeX
+  TEXDIR=${TINYTEX_DIR:-"~"}/.TinyTeX
+  if [ $OSNAME != 'Linux']; then
+    TINYTEX_INSTALLER="intaller-unix"
+  fi
 fi
 
 rm -rf $TEXDIR
 
+if [ -z $TINYTEX_VERSION ]; then
+  TINYTEX_URL="https://yihui.org/tinytex/$TINYTEX_INSTALLER"
+else
+  TINYTEX_URL="https://github.com/yihui/tinytex-releases/releases/download/v$TINYTEX_VERSION/$TINYTEX_INSTALLER-v$TINYTEX_VERSION"
+fi
+
 case $OSNAME in
   "Darwin")
-    curl -LO https://yihui.org/tinytex/TinyTeX-1.tgz
+    curl -LO ${TINYTEX_URL}.tgz
     tar xzf TinyTeX-1.tgz -C $(dirname $TEXDIR)
     rm TinyTeX-1.tgz
     ;;
   "Linux")
-    wget --progress=dot:giga https://yihui.org/tinytex/TinyTeX-1.tar.gz
+    wget --progress=dot:giga ${TINYTEX_URL}.tar.gz
     tar xzf TinyTeX-1.tar.gz -C $(dirname $TEXDIR)
     rm TinyTeX-1.tar.gz
     ;;
   *)
     echo "We do not have a prebuilt TinyTeX package for the operating system $(uname)."
     echo "I will try to install from source for you instead."
-    wget https://yihui.org/tinytex/installer-unix.tar.gz
-    tar xzf installer-unix.tar.gz
+    wget ${TINYTEX_URL}.tar.gz
+    tar xzf ${TINYTEX_INSTALLER}.tar.gz
     ./install.sh
     mkdir -p $TEXDIR
     mv texlive/* $TEXDIR
-    rm -r texlive installer-unix.tar.gz install.sh
+    rm -r texlive ${TINYTEX_INSTALLER}.tar.gz install.sh
     ;;
 esac
 
--- tools/install-bin-windows.bat
+++ tools/install-bin-windows.bat
@@ -7,8 +7,18 @@ cd /d "%TEMP%"
 rem in case there is a leftover TinyTeX* dir, delete it
 for /d %%G in ("TinyTeX*") do rd /s /q "%%~G"
 
+if %TINYTEX_INSTALLER% == "" (
+  set TINYTEX_INSTALLER=TinyTeX-1
+)
+
+if %TINYTEX_VERSION% == "" (
+  set TINYTEX_URL=https://yihui.org/tinytex/%TINYTEX_INSTALLER%
+) else (
+  set TINYTEX_URL=https://github.com/yihui/tinytex-releases/releases/download/v%TINYTEX_VERSION%/%TINYTEX_INSTALLER%-v%TINYTEX_VERSION%
+)
+
 rem download the zip package and unzip it
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://yihui.org/tinytex/TinyTeX-1.zip -OutFile install.zip"
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $Env:TINYTEX_URL.zip -OutFile install.zip"
 powershell -Command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('install.zip', '.'); }"
 del install.zip
 
