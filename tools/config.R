ce0e9d311830673dbfd2aa945145c54c45020b7f
Yihui Xie <xie@yihui.name>
2020-10-04 10:09:57 -0500

    always set R_LIBS_USER on *nix

--- tools/config.R
+++ tools/config.R
@@ -1,5 +1,5 @@
 writeLines('options(repos = "https://cran.rstudio.com")', '~/.Rprofile')
-if (Sys.getenv('R_LIBS_USER') == '') {
+if (.Platform$OS.type == 'unix' || Sys.getenv('R_LIBS_USER') == '') {
   dir.create('~/R', FALSE, TRUE)
   writeLines('R_LIBS_USER="~/R"', '~/.Renviron')
 }
