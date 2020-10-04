07dc5d21732dcfb8fa18844faf16c30d29fb8d1a
Yihui Xie <xie@yihui.name>
2020-10-04 09:44:56 -0500

    don't set R_LIBS_USER if it has been set

--- tools/config.R
+++ tools/config.R
@@ -1,3 +1,5 @@
 writeLines('options(repos = "https://cran.rstudio.com")', '~/.Rprofile')
-dir.create('~/R', FALSE, TRUE)
-writeLines('R_LIBS_USER="~/R"', '~/.Renviron')
+if (Sys.getenv('R_LIBS_USER') == '') {
+  dir.create('~/R', FALSE, TRUE)
+  writeLines('R_LIBS_USER="~/R"', '~/.Renviron')
+}
