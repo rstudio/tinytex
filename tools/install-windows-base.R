db24cf82a2598781fc08cfa97c34b40537416010
Yihui Xie <xie@yihui.name>
2020-10-08 07:37:10 -0500

    remove the patch introduced in d9ed3c452bbcb526ad682b18a99207d084ce1dbd; the problem seesm to have been fixed upstream

--- tools/install-windows-base.R
+++ tools/install-windows-base.R
@@ -10,16 +10,6 @@ x = x[!i]
 x = x[x != 'pause']  # do not pause
 writeLines(x, f)
 shell('install-windows.bat')
-
-# patch tlmgr.pl to avoid the warning 'Use of uninitialized value in bitwise or
-# (|) at...\TinyTeX\texmf-dist\scripts\texlive\tlmgr.pl line 1510'
-# http://www.tug.org/svn/texlive/trunk/Master/texmf-dist/scripts/texlive/tlmgr.pl?r1=56562&r2=56561&pathrev=56562
-f = file.path(Sys.getenv('APPDATA'), 'TinyTeX/texmf-dist/scripts/texlive/tlmgr.pl')
-x = readLines(f)
-i = grep('^\\s*\\$ret \\|= TeXLive::TLWinGoo::broadcast_env\\();', x)
-if (length(i)) x = x[-i] else stop('No need to patch tlmgr.pl on Windows any more.')
-writeLines(x, f)
-
 setwd(owd)
 
 sys.source('tools/clean-tlpdb.R')
