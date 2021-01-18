0335b635959aaaa8e9143aaec1d6ff84aad844fb
christophe dervieux <christophe.dervieux@gmail.com>
2021-01-18 19:05:30 +0100

    add test with tlmgr to check the font is correctly found in a package

--- tests/test-travis/test-tlmgr.R
+++ tests/test-travis/test-tlmgr.R
@@ -16,3 +16,10 @@ assert('`tlmgr info` can list the installed packages', {
   # only check a few basic packages
   (c('xetex', 'luatex', 'graphics') %in% res)
 })
+
+suppressMessages(
+  assert('fonts package are correctly identified', {
+    parse_packages(text = "! Font U/psy/m/n/10=psyr at 10.0pt not loadable: Metric (TFM) file not found") %==% "symbol"
+    parse_packages(text = '! Package fontspec Error: The font "Caladea" cannot be found.') %==% 'caladea'
+  })
+)
