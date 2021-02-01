c4b411b771013b457889c03b80eb615a16f142f9
christophe dervieux <christophe.dervieux@gmail.com>
2021-02-01 20:15:13 +0100

    Try 3 download methods until none failed
    and returns the correct exit code

--- tools/install-bin-windows.bat
+++ tools/install-bin-windows.bat
@@ -15,8 +15,23 @@ if not defined TINYTEX_VERSION (
   set TINYTEX_URL=https://github.com/yihui/tinytex-releases/releases/download/v%TINYTEX_VERSION%/%TINYTEX_INSTALLER%-v%TINYTEX_VERSION%.zip
 )
 
-rem download the zip package and unzip it
+rem download the zip package - method 1
+echo Download zip file... Method 1
+powershell -Command "& { try {Add-Type -A 'System.Net.Http'; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;$response = (New-Object System.Net.Http.HttpClient).GetAsync($Env:TINYTEX_URL); $response.Wait(); $outputFileStream = [System.IO.FileStream]::new('install.zip', [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write); $response.Result.Content.CopyToAsync($outputFileStream).Wait(); $outputFileStream.Close()} catch {throw $_}}"
+if ,not errorlevel 1 goto unzip
+
+rem Try another method if the first one failed
+echo Download zip file... Method 2
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;(New-Object System.Net.WebClient).DownloadFile($Env:TINYTEX_URL, 'install.zip')"
+if not errorlevel 1 goto :unzip
+
+rem Try last method
+echo Download zip file... Method 3
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $Env:TINYTEX_URL -OutFile install.zip"
+if errorlevel 1 exit /b %ERRORLEVEL%
+
+:unzip
+rem unzip the downloaded file
 powershell -Command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('install.zip', '.'); }"
 del install.zip
 
@@ -26,4 +41,4 @@ move /y TinyTeX "%APPDATA%"
 
 call "%APPDATA%\TinyTeX\bin\win32\tlmgr" path add
 
-pause
+exit /b %ERRORLEVEL%
