0f40cdbd6e939fda91f859500075450a50e5cfd9
christophe dervieux <christophe.dervieux@gmail.com>
2021-02-01 15:17:59 +0100

    Retry download with Powershell if it has error

--- tools/install-bin-windows.bat
+++ tools/install-bin-windows.bat
@@ -15,8 +15,23 @@ if not defined TINYTEX_VERSION (
   set TINYTEX_URL=https://github.com/yihui/tinytex-releases/releases/download/v%TINYTEX_VERSION%/%TINYTEX_INSTALLER%-v%TINYTEX_VERSION%.zip
 )
 
-rem download the zip package and unzip it
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $Env:TINYTEX_URL -OutFile install.zip"
+rem download the zip package - retry 5 times if issues
+set /a retry=0
+:download
+if %retry% leq 5 (
+  powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $Env:TINYTEX_URL -OutFile install.zip"
+  if errorlevel 1 (
+    echo Retrying download... attempt %retry%
+    set /a retry+=1
+    goto download
+  )
+  echo Download succeeded
+) else (
+  echo TinyTeX zip installer not downloaded
+  goto :exit
+)
+
+rem unzip it
 powershell -Command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('install.zip', '.'); }"
 del install.zip
 
@@ -27,3 +42,6 @@ move /y TinyTeX "%APPDATA%"
 call "%APPDATA%\TinyTeX\bin\win32\tlmgr" path add
 
 pause
+
+:exit
+exit /b %ERRORLEVEL%
