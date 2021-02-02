b2d1bae772f3f979e77fca9fb5efda05855b39d2
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-02-02 10:46:07 +0100

    Try 3 download methods and returns the correct exit code (#276)
    Installing TinyTeX on GHA using the BAT installed script for binaries has issue that this aims to resolve. 
    `Invoke-Request` can have some issues with heavy binary and could be the cause of the unexpected error in GHA. We now use 3 download method, one after the other, starting with more recent .NET class to use in Powershell. The PR thread gives details about this.
    
    The script also has now some exit code to clearly signal error that could be handled by other tools using this script. (should solve https://github.com/r-lib/actions/issues/213)

--- tools/install-bin-windows.bat
+++ tools/install-bin-windows.bat
@@ -15,15 +15,33 @@ if not defined TINYTEX_VERSION (
   set TINYTEX_URL=https://github.com/yihui/tinytex-releases/releases/download/v%TINYTEX_VERSION%/%TINYTEX_INSTALLER%-v%TINYTEX_VERSION%.zip
 )
 
-rem download the zip package and unzip it
+rem download the zip package - method 1
+echo Download zip file... Method 1
+powershell -Command "& { try {Add-Type -A 'System.Net.Http'; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $response = (New-Object System.Net.Http.HttpClient).GetAsync($Env:TINYTEX_URL); $response.Wait(); $outputFileStream = [System.IO.FileStream]::new('install.zip', [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write); $response.Result.Content.CopyToAsync($outputFileStream).Wait(); $outputFileStream.Close()} catch {throw $_}}"
+if not errorlevel 1 goto unzip
+
+rem Try another method if the first one failed
+echo Download zip file... Method 2
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile($Env:TINYTEX_URL, 'install.zip')"
+if not errorlevel 1 goto unzip
+
+rem Try last method
+echo Download zip file... Method 3
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $Env:TINYTEX_URL -OutFile install.zip"
+if errorlevel 1 exit /b %ERRORLEVEL%
+
+:unzip
+rem unzip the downloaded file
+echo unzip TinyTeX
 powershell -Command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('install.zip', '.'); }"
 del install.zip
 
+echo Move to APPDATA folder
 rd /s /q "%APPDATA%\TinyTeX"
 rd /s /q "%APPDATA%\TinyTeX"
 move /y TinyTeX "%APPDATA%"
 
+echo add tlmgr to PATH
 call "%APPDATA%\TinyTeX\bin\win32\tlmgr" path add
 
-pause
+exit /b %ERRORLEVEL%
