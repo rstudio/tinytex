cfce3b3a13d3486e39cf2facd15a48035b049e2f
Yihui Xie <xie@yihui.name>
2021-03-06 08:17:08 -0600

    add more packages from https://github.com/yihui/crandalf/runs/2046405291

--- tools/sort.R
+++ tools/sort.R
@@ -1,9 +1,9 @@
 local({
   f1 = 'tools/pkgs-custom.txt'
-  x1 = sort(readLines(f1))
+  x1 = sort(scan(f1, 'character'))
   writeLines(x1, f1)
 
   f2 = 'tools/pkgs-yihui.txt'
-  x2 = setdiff(readLines(f2), readLines(f1))
+  x2 = setdiff(scan(f2, 'character'), scan(f1, 'character'))
   writeLines(sort(x2), f2)
 })
