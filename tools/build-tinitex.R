0bd00095abf7729a649682633fc85ca028d00d79
Yihui Xie <xie@yihui.name>
2021-05-26 09:56:20 -0500

    rename the `tinytex` binary to `tinitex` to avoid confusion: tinytex is the R package name, and tinitex will be the command name

---
+++ tools/build-tinitex.R
@@ -0,0 +1,28 @@
+system2('git', c('clone', '--depth=1', 'https://github.com/quarto-dev/quarto-cli'))
+
+setwd('quarto-cli')
+
+os = if (.Platform$OS.type == 'windows') 1 else if (Sys.info()['sysname'] == 'Darwin') 2 else 3
+
+system2(c('configure-windows.cmd', './configure-macos.sh', './configure-linux.sh')[os])
+
+setwd('package/src')
+system2(
+  paste0(if (os > 1) './', 'quarto-bld'),
+  c(
+    'compile-quarto-latexmk', '--name', 'tinitex', '', '--version', format(Sys.Date(), '%Y.%m'),
+    '--description', 'Intelligent compilation of LaTeX documents with TinyTeX or TeX Live', '--target',
+    c('x86_64-pc-windows-msvc', 'x86_64-apple-darwin', 'x86_64-unknown-linux-gnu')[os]
+  )
+)
+
+b = list.files('../dist/bin/tinitex', '^tinitex([.]exe)?$', full.names = TRUE, recursive = TRUE)
+message('tinitex was built at ', b)
+Sys.chmod(b, '0755')
+if (os == 1) {
+  zip(p <- 'tinitex.zip', b)
+} else {
+  tar(p <- paste0('tinitex.', if (os == 2) 'tgz' else 'tar.gz'), b, compression = 'gzip')
+}
+
+file.copy(p, '../../..')
