fbc3cb1514845bfe3a4ce61b2f9a9057da101d46
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-11-25 13:16:11 +0100

    Only get first line of output

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,7 @@ xfun_session_info = function() {
 
   info = if (is.null(pdftex_info)) {
     # try tectonic engine ?
-    tryCatch(system2('tectonic', "--version", stdout = TRUE), error = function(e) NULL)
+    tryCatch(system2('tectonic', "--version", stdout = TRUE)[1], error = function(e) NULL)
   } else if (grepl("TeX Live", pdftex_info, fixed = TRUE)) {
     # we get more information on tlmgr in that case
     tryCatch(tlmgr_version(raw = FALSE), error = function(e) NULL)
