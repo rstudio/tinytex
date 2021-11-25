f5f45b23dd2c34195c2801f468d1cbeabf76da38
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-11-25 13:01:56 +0100

    Better handling of early return

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,19 @@
 # for xfun::session_info('tinytex')
 xfun_session_info = function() {
   pdftex_info = tryCatch(system2('pdflatex', "--version", stdout = TRUE)[1], error = function(e) NULL)
+  if (is.null(pdftex_info)) return(invisible(NULL))
+
   info = if (grepl("TeX Live", pdftex_info, fixed = TRUE)) {
+    # we get more information on tlmgr in that case
     tryCatch(tlmgr_version(raw = FALSE), error = function(e) NULL)
   } else {
-    # e.g MiKTeX-pdfTeX 4.8 (MiKTeX 21.8) return MiKTeX 21.8
+    # For other distributions
+    # e.g MiKTeX-pdfTeX 4.8 (MiKTeX 21.8) returns MiKTeX 21.8
     xfun::grep_sub("^.*\\((.*)\\)$", "\\1", pdftex_info)
   }
 
-  if (length(info)) {
-    return(paste(c("LaTeX version used: ", paste0("  ", info)), collapse = "\n"))
-  }
-  invisible(NULL)
+  if (!length(info)) return(invisible(NULL))
+
+  paste(c("LaTeX version used: ", paste0("  ", info)), collapse = "\n")
 }
 
