2f7afec54a9128638c5688e4ba5e147beec892b9
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-11-25 12:53:59 +0100

    Handle MikTeX and possibly other distribution in session_info()

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,16 @@
 # for xfun::session_info('tinytex')
 xfun_session_info = function() {
-  tryCatch(tlmgr_version(raw = FALSE), error = function(e) NULL)
+  pdftex_info = tryCatch(system2('pdflatex', "--version", stdout = TRUE)[1], error = function(e) NULL)
+  info = if (grepl("TeX Live", pdftex_info, fixed = TRUE)) {
+    tryCatch(tlmgr_version(raw = FALSE), error = function(e) NULL)
+  } else {
+    # e.g MiKTeX-pdfTeX 4.8 (MiKTeX 21.8) return MiKTeX 21.8
+    xfun::grep_sub("^.*\\((.*)\\)$", "\\1", pdftex_info)
+  }
+
+  if (length(info)) {
+    return(paste(c("LaTeX version used: ", paste0("  ", info)), collapse = "\n"))
+  }
+  invisible(NULL)
 }
 
