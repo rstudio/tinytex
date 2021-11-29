adfc8eda43ac7907861eaa66f95694c94fcbb9e8
Yihui Xie <xie@yihui.name>
2021-11-29 13:38:06 -0600

    factor out some code into try_null() and version_info()

--- R/utils.R
+++ R/utils.R
@@ -1,21 +1,20 @@
 # for xfun::session_info('tinytex')
 xfun_session_info = function() {
-  pdftex_info = tryCatch(system2('pdflatex', "--version", stdout = TRUE)[1], error = function(e) NULL)
+  try_null = function(...) tryCatch(..., error = function(e) NULL)
+  version_info = function(cmd) try_null(system2(cmd, '--version', stdout = TRUE))
+  pdftex_info = version_info('pdflatex')[1]
 
   info = if (is.null(pdftex_info)) {
-    # try tectonic engine ?
-    tryCatch(system2('tectonic', "--version", stdout = TRUE)[1], error = function(e) NULL)
-  } else if (grepl("TeX Live", pdftex_info, fixed = TRUE)) {
+    version_info('tectonic')[1]  # try tectonic engine?
+  } else if (grepl('TeX Live', pdftex_info, fixed = TRUE)) {
     # we get more information on tlmgr in that case
-    tryCatch(tlmgr_version(raw = FALSE), error = function(e) NULL)
+    try_null(tlmgr_version(raw = FALSE))
   } else {
-    # For other distributions
-    # e.g MiKTeX-pdfTeX 4.8 (MiKTeX 21.8) returns MiKTeX 21.8
-    xfun::grep_sub("^.*\\((.*)\\)$", "\\1", pdftex_info)
+    # for other distributions, e.g., MiKTeX-pdfTeX 4.8 (MiKTeX 21.8)
+    xfun::grep_sub('^.*\\((.*)\\)$', '\\1', pdftex_info)
   }
-
   if (!length(info)) return(invisible(NULL))
 
-  paste(c("LaTeX version used: ", paste0("  ", info)), collapse = "\n")
+  paste(c('LaTeX version used: ', paste0('  ', info)), collapse = '\n')
 }
 
