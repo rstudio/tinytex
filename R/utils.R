3041a6cec0d2e0d6515e6b11b7624d513a78cca9
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-11-29 20:49:01 +0100

    close #342: handle MikTeX and possibly other distribution in xfun::session_info() (#343)

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,21 @@
 # for xfun::session_info('tinytex')
 xfun_session_info = function() {
-  tryCatch(tlmgr_version(raw = FALSE), error = function(e) NULL)
+  try_null = function(...) tryCatch(..., error = function(e) NULL)
+  version_info = function(cmd) try_null(system2(cmd, '--version', stdout = TRUE))
+  tweak_path()
+  pdftex_info = version_info('pdflatex')[1]
+
+  info = if (is.null(pdftex_info)) {
+    version_info('tectonic')[1]  # try tectonic engine?
+  } else if (grepl('TeX Live', pdftex_info, fixed = TRUE)) {
+    # we get more information on tlmgr in that case
+    try_null(tlmgr_version(raw = FALSE))
+  } else {
+    # for other distributions, e.g., MiKTeX-pdfTeX 4.8 (MiKTeX 21.8)
+    xfun::grep_sub('^.*\\((.*)\\)$', '\\1', pdftex_info)
+  }
+  if (!length(info)) return(invisible(NULL))
+
+  paste(c('LaTeX version used: ', paste0('  ', info)), collapse = '\n')
 }
 
