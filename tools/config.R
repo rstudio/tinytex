8df65f0e452c35163ab6d88d407c2f1e14c92f72
Yihui Xie <xie@yihui.name>
2021-12-14 16:05:25 -0600

    revert 72d7948a63f00e0319cd7c05b98c23945aa51cc2: don't use r-universe.dev any more (the new version of xfun is on CRAN now)

--- tools/config.R
+++ tools/config.R
@@ -1,4 +1,4 @@
-writeLines('options(repos = c("https://yihui.r-universe.dev", "https://cran.rstudio.com"))', '~/.Rprofile')
+writeLines('options(repos = "https://cran.rstudio.com")', '~/.Rprofile')
 if (.Platform$OS.type == 'unix' || Sys.getenv('R_LIBS_USER') == '') {
   dir.create('~/R', FALSE, TRUE)
   writeLines('R_LIBS_USER="~/R"', '~/.Renviron')
