f3243f686769cb148de0767001f92eb68ba65779
Yihui Xie <xie@yihui.name>
2021-12-18 23:22:59 -0600

    hide the tlmgr() examples from R CMD check (not sure why it takes so long on CRAN's Windows machine)

--- R/tlmgr.R
+++ R/tlmgr.R
@@ -27,15 +27,15 @@
 #' @references The \pkg{tlmgr} manual:
 #'   \url{https://www.tug.org/texlive/doc/tlmgr.html}
 #' @export
-#' @examples
+#' @examplesIf interactive()
 #' # search for a package that contains titling.sty
 #' tlmgr_search('titling.sty')
 #'
-#' #' to match titling.sty exactly, add a slash before the keyword, e.g.
-#' #' tlmgr_search('/titling.sty')
+#' # to match titling.sty exactly, add a slash before the keyword, e.g.
+#' tlmgr_search('/titling.sty')
 #'
-#' #' use a regular expression if you want to be more precise, e.g.
-#' #' tlmgr_search('/titling\\.sty$')
+#' # use a regular expression if you want to be more precise, e.g.
+#' tlmgr_search('/titling\\.sty$')
 #'
 #' # list all installed LaTeX packages
 #' tlmgr(c('info', '--list', '--only-installed', '--data', 'name'))
