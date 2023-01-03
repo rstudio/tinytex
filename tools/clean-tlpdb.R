8ee79e313d544023f26d87910882f63ec203876d
Yihui Xie <xie@yihui.name>
2023-01-03 14:47:32 -0600

    compress executables via upx (see if it helps #394)

--- tools/clean-tlpdb.R
+++ tools/clean-tlpdb.R
@@ -1,7 +1,21 @@
 if (!requireNamespace('tinytex', quietly = TRUE)) utils::install.packages('tinytex')
 # clean texlive.tlpdb.* files
 tinytex:::delete_tlpdb_files()
-# clean log files
-unlink(list.files(
-  tinytex::tinytex_root(FALSE), '[.]log$', full.names = TRUE, recursive = TRUE
-))
+
+local({
+  files = list.files(tinytex::tinytex_root(FALSE), full.names = TRUE, recursive = TRUE)
+
+  # clean log files
+  unlink(grep('[.]log$', files, value = TRUE))
+
+  # compress executables using upx
+  if (Sys.which('upx') != '') {
+    if (xfun::is_windows()) {
+      files = grep('[.](dll|exe)$', files, value = TRUE)
+    } else {
+      files = files[file_test('-x', files)]
+      files = files[sapply(sprintf('file --mime-encoding %s | grep -q binary', shQuote(files)), system) == 0]
+    }
+    for (f in shQuote(files)) system2('upx', c('-qq', f))
+  }
+})
