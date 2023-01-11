60587ad726b4b6278717ca7b8ac511eb7ef75513
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-01-11 11:56:43 +0100

    Build TinyTex-2 on windows as self extracting archive using sfx (#398)
    To reduce size of bundles uploaded in tinytex-releases as we are close to limit already

--- tools/install-bin-windows.bat
+++ tools/install-bin-windows.bat
@@ -9,32 +9,41 @@ for /d %%G in ("TinyTeX*") do rd /s /q "%%~G"
 
 if not defined TINYTEX_INSTALLER set TINYTEX_INSTALLER=TinyTeX-1
 
+set BUNDLE_EXT=zip
+if "%TINYTEX_INSTALLER%" == "TinyTeX-2" set BUNDLE_EXT=exe
+
 if not defined TINYTEX_VERSION (
-  set TINYTEX_URL=https://github.com/rstudio/tinytex-releases/releases/download/daily/%TINYTEX_INSTALLER%.zip
+  set TINYTEX_URL=https://github.com/rstudio/tinytex-releases/releases/download/daily/%TINYTEX_INSTALLER%.%BUNDLE_EXT%
 ) else (
-  set TINYTEX_URL=https://github.com/rstudio/tinytex-releases/releases/download/v%TINYTEX_VERSION%/%TINYTEX_INSTALLER%-v%TINYTEX_VERSION%.zip
+  set TINYTEX_URL=https://github.com/rstudio/tinytex-releases/releases/download/v%TINYTEX_VERSION%/%TINYTEX_INSTALLER%-v%TINYTEX_VERSION%.%BUNDLE_EXT%
 )
 
+set DOWNLOADED_FILE=install.%BUNDLE_EXT%
+
 rem download the zip package - method 1
-echo Download zip file... Method 1
-powershell -Command "& { try {Add-Type -A 'System.Net.Http'; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $response = (New-Object System.Net.Http.HttpClient).GetAsync($Env:TINYTEX_URL); $response.Wait(); $outputFileStream = [System.IO.FileStream]::new('install.zip', [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write); $response.Result.Content.CopyToAsync($outputFileStream).Wait(); $outputFileStream.Close()} catch {throw $_}}"
+echo Download %BUNDLE_EXT% file... Method 1
+powershell -Command "& { try {Add-Type -A 'System.Net.Http'; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $response = (New-Object System.Net.Http.HttpClient).GetAsync($Env:TINYTEX_URL); $response.Wait(); $outputFileStream = [System.IO.FileStream]::new($Env:DOWNLOADED_FILE, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write); $response.Result.Content.CopyToAsync($outputFileStream).Wait(); $outputFileStream.Close()} catch {throw $_}}"
 if not errorlevel 1 goto unzip
 
 rem Try another method if the first one failed
-echo Download zip file... Method 2
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile($Env:TINYTEX_URL, 'install.zip')"
+echo Download %BUNDLE_EXT% file... Method 2
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile($Env:TINYTEX_URL, $Env:DOWNLOADED_FILE)"
 if not errorlevel 1 goto unzip
 
 rem Try last method
-echo Download zip file... Method 3
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $Env:TINYTEX_URL -OutFile install.zip"
+echo Download bundle file... Method 3
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest $Env:TINYTEX_URL -OutFile $Env:DOWNLOADED_FILE"
 if errorlevel 1 exit /b %ERRORLEVEL%
 
 :unzip
 rem unzip the downloaded file
-echo unzip TinyTeX
-powershell -Command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('install.zip', '.'); }"
-del install.zip
+echo Unbundle TinyTeX
+if %BUNDLE_EXT% == exe (
+  CALL %DOWNLOADED_FILE% -y
+) ELSE (
+  powershell -Command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory($Env:DOWNLOADED_FILE, '.'); }"
+)
+del %DOWNLOADED_FILE%
 
 echo Move to APPDATA folder
 rd /s /q "%APPDATA%\TinyTeX"
