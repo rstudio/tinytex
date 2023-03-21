976de8cf2556aaa1b5af6409c7762531de07e2c9
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-03-21 20:39:51 +0100

    add 'windows' and remove 'i386-cygwin' in the list of platforms (#409)
    Co-authored-by: Yihui Xie <xie@yihui.name>

--- R/tlmgr.R
+++ R/tlmgr.R
@@ -361,9 +361,9 @@ tl_platforms = function(print = FALSE) {
 # a copy of the returned result from tl_platform() is saved here because
 # tl_platform() is a little slow and requires Internet connection
 .tl_platforms = c(
-  'aarch64-linux', 'amd64-freebsd', 'amd64-netbsd', 'armhf-linux', 'i386-cygwin',
+  'aarch64-linux', 'amd64-freebsd', 'amd64-netbsd', 'armhf-linux',
   'i386-freebsd', 'i386-linux', 'i386-netbsd', 'i386-solaris', 'universal-darwin',
-  'win32', 'x86_64-cygwin', 'x86_64-darwinlegacy', 'x86_64-linux',
+  'windows', 'x86_64-cygwin', 'x86_64-darwinlegacy', 'x86_64-linux',
   'x86_64-linuxmusl', 'x86_64-solaris'
 )
 
