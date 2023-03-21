b948609ef2e77c0cd9c897b4b1d2b44bfaab53e8
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-03-21 18:03:28 +0100

    Add windows to the list of platforms

--- R/tlmgr.R
+++ R/tlmgr.R
@@ -360,10 +360,11 @@ tl_platforms = function(print = FALSE) {
 
 # a copy of the returned result from tl_platform() is saved here because
 # tl_platform() is a little slow and requires Internet connection
+# also win32 (< TeX Live 2023) and windows (>= Tex Live 2023) are both added
 .tl_platforms = c(
   'aarch64-linux', 'amd64-freebsd', 'amd64-netbsd', 'armhf-linux', 'i386-cygwin',
   'i386-freebsd', 'i386-linux', 'i386-netbsd', 'i386-solaris', 'universal-darwin',
-  'win32', 'x86_64-cygwin', 'x86_64-darwinlegacy', 'x86_64-linux',
+  'windows', 'win32', 'x86_64-cygwin', 'x86_64-darwinlegacy', 'x86_64-linux',
   'x86_64-linuxmusl', 'x86_64-solaris'
 )
 
