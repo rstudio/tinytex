95e0272a6576f29099188bfc08c8ea3e27e12889
Yihui Xie <xie@yihui.name>
2026-03-06 23:12:40 -0600

    make sure scheme-full is installed

--- tools/check-update.R
+++ tools/check-update.R
@@ -1,5 +1,10 @@
+# check if we need to update the downloaded TinyTeX-2 bundle
 u = grep('^update: ', tinytex::tlmgr(c('update', '--list'), stdout = TRUE), value = TRUE)
 if (length(u)) {
   message(paste(c('Needs update:\n', u), collapse = '\n'))
+  quit(status = 1)
+}
+if (!tinytex::check_installed('scheme-full')) {
+  message('Needs update: scheme-full is not installed')
   quit(status = 1)
 }
