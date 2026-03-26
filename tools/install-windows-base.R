ae949dc22080861215de4569a24c100b81090480
Yihui Xie <xie@yihui.name>
2026-03-26 11:25:16 -0500

    we are not calling .\tlmgr.bat now

--- tools/install-windows-base.R
+++ tools/install-windows-base.R
@@ -3,13 +3,13 @@
 owd = setwd('tools')
 f = 'install-windows.ps1'
 x = readLines(f)
-i = x == r"(& .\tlmgr.bat install @pkgs)"
+i = x == r"(& .\tlmgr install @pkgs)"
 if (sum(i) != 1)
   stop('The script ', f, ' should contain a line to install extra LaTeX packages.')
 x = x[!i]
 x = x[x != 'pause']  # do not pause
 writeLines(x, f)
-shell('powershell -ExecutionPolicy Bypass -File install-windows.ps1')
+shell(paste('powershell -ExecutionPolicy Bypass -File', f))
 setwd(owd)
 
 sys.source('tools/clean-tlpdb.R')
